0fb9e2ee11a64a3cc106611664c5bb875fda9ddd
krinya32 <krinya32@mail.ru>
2022-05-03 14:01:46 +0300

    Working with API

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,50 @@
 package main
 
 import (
+	"github.com/joho/godotenv"
 	"github.com/krinya32/todoApp"
-	handlers2 "github.com/krinya32/todoApp/pkg/handlers"
+	"github.com/krinya32/todoApp/pkg/handler"
+	"github.com/krinya32/todoApp/pkg/repository"
+	"github.com/krinya32/todoApp/pkg/service"
+	_ "github.com/lib/pq"
+	"github.com/spf13/viper"
 	"log"
+	"os"
 )
 
 func main() {
-	handlers := new(handlers2.Handler)
+	if err := InitConfig(); err != nil {
+		log.Fatalf("Error initializing configs: %s", err.Error())
+	}
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("Error loading env variables: %s", err.Error())
+	}
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		DBname:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+		Password: os.Getenv("DB_PASSWORD"),
+	})
+	if err != nil {
+		log.Fatalf("Failed to initialize db:%s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 
 	srv := new(todoApp.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
-		log.Fatal("Error occured while running http server: %s", err.Error())
+	if err := srv.Run(viper.GetString("8000"), handlers.InitRoutes()); err != nil {
+		log.Fatalf("Error occured while running http server: %s", err.Error())
 	}
+}
+
+func InitConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
