f63e74590439cbf54f7ede33e764650bba60ed0e
krinya32 <krinya32@mail.ru>
2022-05-05 22:24:29 +0300

    fix bugs

--- pkg/handler/auth.go
+++ pkg/handler/auth.go
@@ -18,7 +18,6 @@ func (h *Handler) signUp(c *gin.Context) {
 		newErrorResponse(c, http.StatusInternalServerError, err.Error())
 		return
 	}
-
 	c.JSON(http.StatusOK, map[string]interface{}{
 		"id": id,
 	})
